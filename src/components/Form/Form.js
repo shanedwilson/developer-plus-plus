77f91a0fe77f76c75fb00363a1600ec482d0a13e
Shane D Wilson <me@shanedwilson.com>
2018-12-20 14:50:30 -0600

    reading input value in dev tools

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,27 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import authRequests from '../../helpers/data/authRequests';
+
 import './Form.scss';
 
+const defaultItem = {
+  name: '',
+  url: '',
+  uid: '',
+  type: '',
+};
+
+
 class Form extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  }
+
+  state = {
+    newItem: defaultItem,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempItem = { ...this.state.newItem };
+    tempItem[name] = e.target.value;
+    this.setState({ newItem: tempItem });
+  }
+
+  nameChange = e => this.formFieldStringState('name', e);
+
+  urlChange = e => this.formFieldStringState('url', e);
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const myItem = { ...this.state.newItem };
+    myItem.uid = authRequests.getCurrentUid();
+    onSubmit(myItem);
+    this.setState({ newItem: defaultItem });
+  }
+
   render() {
+    const { newItem } = this.state;
+
     return (
       <div className="row form-container mx-auto border border-dark rounded mt-5">
-        <div className="form col-8 mt-4">
+        <div className="form col-8 mt-4" onSubmit={this.formSubmit}>
           <div className="col-auto">
-            <label className="sr-only">Name</label>
+            <label htmlFor="name" className="sr-only">Name</label>
             <div className="input-group mb-2">
               <div className="input-group-prepend">
                 <div className="input-group-text">Name</div>
               </div>
-              <input type="text" className="form-control" id="name-input" placeholder="Mr. Rogers"></input>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Mr. Rogers"
+                onChange={this.nameChange}
+                value={newItem.name}>
+              </input>
             </div>
           </div>
           <div className="col-auto">
-            <label className="sr-only">Link</label>
+            <label htmlFor="link" className="sr-only">Link</label>
             <div className="input-group mb-2">
               <div className="input-group-prepend">
                 <div className="input-group-text">Link</div>
               </div>
-              <input type="text" className="form-control" id="link-input" placeholder="www.shityeah.com"></input>
+              <input
+                type="text"
+                className="form-control"
+                id="url"
+                placeholder="www.shityeah.com"
+                onChange={this.urlChange}
+                value={newItem.url}>
+              </input>
             </div>
           </div>
         </div>
