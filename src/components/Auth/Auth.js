cfbed3aadaae3e6c2277af36a0863cf7f79848a2
Shane D Wilson <me@shanedwilson.com>
2018-12-29 17:18:48 -0600

    styling

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -20,8 +20,8 @@ class Auth extends React.Component {
   render() {
     return (
       <div className="Auth">
-        <button className="btn btn-danger" onClick={this.authenticateUser}>
-          <img src="https://help.dropsource.com/wp-content/uploads/sites/4/2017/02/gh-login-button.png" alt="github login"></img>
+        <button className="btn" onClick={this.authenticateUser}>
+          <img className="login-img" src="https://help.dropsource.com/wp-content/uploads/sites/4/2017/02/gh-login-button.png" alt="github login"></img>
         </button>
       </div>
     );
